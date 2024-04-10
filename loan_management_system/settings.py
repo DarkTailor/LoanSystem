bda01abe38fa54fd5d27f8c33324a3504e161223
Aobakwe Mpatane <102150960+DarkTailor@users.noreply.github.com>
2024-04-10 11:45:30 +0200

    Update settings.py

--- loan_management_system/settings.py
+++ loan_management_system/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = '0k7(=3xy503l5nio^)k29b1ryxyfm)r%k)))4xqq3li4)og%9c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['vercel.com/darktailors-projects/loan-system']
+ALLOWED_HOSTS = ['loan-system-two.vercel.app']
 
 
 # Application definition
